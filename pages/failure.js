00aa5deb65a28aaafd1272f9cf561e6310176616
Sean Perkins <sean@mobility-labs.com>
2022-08-09 19:57:00 -0400

    Fix errors

--- pages/failure.js
+++ pages/failure.js
@@ -1,3 +1,5 @@
+import Head from 'next/head'
+
 export default function Failure() {
   function handleSubmit(e) {
     e.preventDefault()
@@ -5,18 +7,27 @@ export default function Failure() {
   }
   return (
     <main>
+      <Head>
+        <title>RVA.js - Failure</title>
+        <meta name="description" content="RVA.js - Failure" />
+      </Head>
       <div>
         <h1>Failure</h1>
-        <img src="https://via.placeholder.com/350x150" />
+        <img src="https://via.placeholder.com/350x150" alt="placeholder" />
         <form onSubmit={handleSubmit}>
-          <label>Name</label>
-          <input
-            type="checkbox"
-            placeholder="Enter your name"
-            name="name"
-            id="name"
-          />
-          <textarea name="textarea" id="textarea" cols="30" rows="10" />
+          <label>
+            Name
+            <input
+              type="text"
+              placeholder="Enter your name"
+              name="name"
+              id="name"
+            />
+          </label>
+          <label>
+            Text area
+            <textarea name="textarea" id="textarea" cols="30" rows="10" />
+          </label>
           <button type="submit">Submit</button>
         </form>
       </div>
