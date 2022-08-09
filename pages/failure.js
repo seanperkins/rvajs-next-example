9b607269d929925ba8d8fb6cf16f0c743807ae83
Sean Perkins <sean@mobility-labs.com>
2022-08-09 18:29:41 -0400

    Add htmlcs runner to pa11y

--- pages/failure.js
+++ pages/failure.js
@@ -4,14 +4,22 @@ export default function Failure() {
     console.log('submit', e)
   }
   return (
-    <div>
-      <h1>Failure</h1>
-      <img src="https://via.placeholder.com/350x150" />
-      <form onSubmit={handleSubmit}>
-        <label>Name</label>
-        <input type="text" placeholder="Enter your name" name="name" />
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+    <main>
+      <div>
+        <h1>Failure</h1>
+        <img src="https://via.placeholder.com/350x150" />
+        <form onSubmit={handleSubmit}>
+          <label>Name</label>
+          <input
+            type="checkbox"
+            placeholder="Enter your name"
+            name="name"
+            id="name"
+          />
+          <textarea name="textarea" id="textarea" cols="30" rows="10" />
+          <button type="submit">Submit</button>
+        </form>
+      </div>
+    </main>
   )
 }
