5f40e096df19dd7054cfe9df2952664514d41bb1
Sean Perkins <sean@mobility-labs.com>
2022-08-09 16:55:21 -0400

    Add a form item missing label

--- pages/failure.js
+++ pages/failure.js
@@ -1,8 +1,16 @@
 export default function Failure() {
+  function handleSubmit(e) {
+    e.preventDefault()
+    console.log('submit', e)
+  }
   return (
     <div>
       <h1>Failure</h1>
       <img src="https://via.placeholder.com/350x150" />
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder="Enter your name" name="name" />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   )
 }
